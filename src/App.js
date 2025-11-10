991af17d24198bbbffd60839a179b7570810ebc9
SpherenexLabs <spherenexgpt@gmail.com>
2025-11-10 14:02:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Neural_Power from './Components/Neural_power'
-import Avr from './Components/Avr'
+// import Neural_Power from './Components/Neural_power'
+// import Avr from './Components/Avr'
 import Solar_Power from './Components/Solar_Power'
 
 function App() {
